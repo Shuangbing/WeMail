9218e96a05636546092cbc43b4465ef00aadd438
Shuangbing <he.shuangbing@gmail.com>
2019-07-05 12:02:57 +0900

    add domain.js/user.js#

---
+++ wemail-web/src/components/Menu.vue
@@ -0,0 +1,15 @@
+import Mail from './components/Mail.vue'
+
+<template>
+<el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-container>
+      <el-main><Mail /></el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
+</el-container>
+</template>
+
