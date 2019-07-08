3083570ffd2dd766226c97659f241f782f28937b
Shuangbing <he.shuangbing@gmail.com>
2019-07-08 20:18:42 +0900

    add register

--- index.js
+++ index.js
@@ -8,7 +8,12 @@ require('./config/database')(app)
 require('./route')(app)
 require('./plugin/mailin')(app)
 
+app.use(async (err, req, res, next) => {
+    res.status(err.statusCode || 500).send({
+      message: err.message
+    })
+})
 
 app.listen(3005, function () {
-    console.log('listening on port 3000!');
-});
+    console.log('listening on port 3000!')
+})
--- route/index.js
+++ route/index.js
@@ -1,17 +1,30 @@
 module.exports = app => {
     const router = require('express').Router()
+    const assert = require('http-assert')
     const mongoose = require('mongoose')
+    const Users = mongoose.model('User')
     const Mails = mongoose.model('Mail')
 
-    router.get('/recive', async function (req, res) {
+    router.get('/mail/recive', async function (req, res) {
         const mails = await Mails.find();
         res.send(mails);
-    });
+    })
     
-    router.get('/recive/:id', async function (req, res) {
+    router.get('/mail/recive/:id', async function (req, res) {
         const mail = await Mails.findById(req.params.id)
         res.send(mail);
-    });
+    })
+
+    router.post('/user/register', async function(req, res){
+        const {username, password} = req.body
+        const user = await Users.create({
+            username: username,
+            password: password
+        })
+        res.send(user)
+    })
     
     app.use('/api', router)
+
+    
 }
